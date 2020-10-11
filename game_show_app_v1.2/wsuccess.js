242081ffe0394d70edbdce8f950808f978239572
Dan Costa <dancosta.id@gmail.com>
2020-10-12 10:27:52 +1300

    Progress being made
    Progress being made. However, I'm stuck with showing the letters.

--- game_show_app_v1.2/wsuccess.js
+++ game_show_app_v1.2/wsuccess.js
@@ -10,7 +10,7 @@ let phrases = [
     'Hire me and see what happens',
     'Life is not about what is better than',
     'Give me a chance to shine',
-    'You are deserve success in life'
+    'You deserve success in life'
 ];
 
 //Getting a random phrase Array
@@ -23,4 +23,60 @@ function getRandomPhraseAsArray(arr){
 
 const phraseArray = (getRandomPhraseAsArray(phrases));
 
-console.log(getRandomPhraseAsArray(phrases));
+// function that loops through an array of characters
+function addPhraseToDisplay(){
+   for (i = 0; i , i < phraseArray.length; i++) {
+       
+        let li = document.createElement('li');
+        let ul = document.querySelector('#phrase ul');
+
+        li.textContent = phraseArray[i];
+        ul.appendChild(li);
+        
+        if (phraseArray[i] != ' ') {
+            li.className = 'letter';
+
+        } else {
+            li.className = 'space';
+        }
+   }
+
+}
+
+addPhraseToDisplay();
+
+//Create a checkLetter function
+const checkLetter = button => {
+    let letters = document.querySelectorAll('.letter');
+    let match = null;
+    
+    for (let i = 0; i < checkLetter.length; i++){
+
+        if (button === letters[i].textContent.toLowerCase()) {
+           letters[i].className = 'show';
+            match = letters[i].textContent;
+        } 
+    }     
+    return match       
+}   
+
+checkLetter ();
+
+// Add an eventListener 
+qwerty.addEventListener('click', (e)=>{
+    if (e.target.tagName === 'BUTTON'){
+        const btnClicked = e.target;
+        btnClicked.className = 'chosen';
+        btnClicked.disabled = 'true';
+
+        let letterFound = checkLetter(btnClicked.textContent);
+        if (letterFound === null){
+            let liveHeart = document.querySelectorAll('.tries');
+            let lostHeart = document.querySelectorAll('.tries img');
+            lostHeart[0].src = "images/lostHeart.png";
+            liveHeart[0].className = ' ';
+        }
+    }
+})
+
+console.log(); 
